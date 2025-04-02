37604435892087dcdf5313e2b23644df808fe03e
jasper tech <curdyj78@gmail.com>
2025-04-02 12:36:30 +0000

    Added Tailwind Utilities

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import Dashboard from "./components/dashboard";
+import ResponsiveDashboard from "./components/responsivedash";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/responsivedashboard" element={<ResponsiveDashboard />} />
       </Routes>
     </Router>
   );
